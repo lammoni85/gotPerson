b5d2179160636738bf8d5fe3dd03a7bc7fba4594
lammoni <lammoni85@gmail.com>
2018-12-08 13:31:59 +0100

    portrék megjelenítése ABC sorrendben

--- got-personal-project/js/got.js
+++ got-personal-project/js/got.js
@@ -13,6 +13,8 @@ function successGetGameOfThronesCharacterDatas(xhttp) {
   // Nem szabad globálisba kitenni a userDatas-t!
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen hívhatod meg a többi függvényed
+  portraitOrderByName(userDatas);
+  portraitsDisplay(userDatas);
 }
 
 getGameOfThronesCharacterDatas(
@@ -22,3 +24,36 @@ getGameOfThronesCharacterDatas(
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+
+function sortByName(one, another) {
+  if (one.name > another.name) {
+    return 1;
+  } else if (one.name < another.name) {
+    return -1;
+  }
+  return 0;
+}
+
+function portraitOrderByName(userDatas) {
+  userDatas.sort(sortByName);
+}
+
+
+function portraitsDisplay(userDatas) {
+  document.querySelector('#portraits').innerHTML = '';
+  var i = 0;
+  while (i < userDatas.length) {
+    if (userDatas.hasOwnProperty(i) && !userDatas[i].dead) {
+      document.querySelector('#portraits').innerHTML +=
+      `
+      <div class="div div--portrait"> 
+      <img src="${userDatas[i].portrait}" alt="${userDatas[i].name}">
+    <p class="portrait--name">
+    ${userDatas[i].name}
+    </p>
+      </div>`;
+    }
+
+    i++;
+  }
+}
